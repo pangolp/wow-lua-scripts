d1f475ef5dd76dfbac12c59edab3d9390a99540f
Pagani Walter <paganiwalter@gmail.com>
2025-10-04 23:49:04 -0300

    tabs for space

--- npc-armas/lua/npc_armas.lua
+++ npc-armas/lua/npc_armas.lua
@@ -70,129 +70,129 @@ function Player:GossipSetText(text, textID)
 end
 
 function OnGossipHello(event, player, object)
-	if (player:IsInCombat() == false) then
-		player:GossipClearMenu()
-		player:GossipSetText("Saludos |c990B0Bee$n|h|r. Bienvenido al Servidor. Mi objetivo es subirte la habilidad de armas hasta el 400. Pero primero necesitas visitar el instructor para aprender dicha habilidad si no la conoces.")
-		if (player:GetLevel() == 80) then
-			if ((player:HasSpell(ARCOS_SPELL) and (player:GetSkillValue(ARCOS_SKILL) ~= 400))) then
-				player:GossipMenuAddItem(0, '  æ Arcos 400 æ  ', 1, 10)
-			end
-			if ((player:HasSpell(DAGAS_SPELL) and (player:GetSkillValue(DAGAS_SKILL) ~= 400))) then
-				player:GossipMenuAddItem(0, '  æ Dagas 400 æ  ', 1, 11)
-			end
-			if ((player:HasSpell(ESPADAS_SPELL) and (player:GetSkillValue(ESPADAS_SKILL) ~= 400))) then
-				player:GossipMenuAddItem(0, '  æ Espadas de 1 mano 400 æ  ', 1, 12)
-			end
-			if ((player:HasSpell(ESPADAS_DE_DOS_MANOS_SPELL) and (player:GetSkillValue(ESPADAS_DE_DOS_MANOS_SKILL) ~= 400))) then
-				player:GossipMenuAddItem(0, '  æ Espadas de 2 manos 400 æ  ', 1, 13)
-			end
-			if ((player:HasSpell(HACHAS_SPELL) and (player:GetSkillValue(HACHAS_SKILL) ~= 400))) then
-				player:GossipMenuAddItem(0, '  æ Hachas de 1 mano 400 æ  ', 1, 14)
-			end
-			if ((player:HasSpell(HACHAS_DE_DOS_MANOS_SPELL) and (player:GetSkillValue(HACHAS_DE_DOS_MANOS_SKILL) ~= 400))) then
-				player:GossipMenuAddItem(0, '  æ Hachas de 2 manos 400 æ  ', 1, 15)
-			end
-			if ((player:HasSpell(DEFENSA_SPELL) and (player:GetSkillValue(DEFENSA_SKILL) ~= 400))) then
-				player:GossipMenuAddItem(0, '  æ Defensa 400 æ  ', 1, 16)
-			end
-			if ((player:HasSpell(ARMA_DE_ASTA_SPELL) and (player:GetSkillValue(ARMA_DE_ASTA_SKILL) ~= 400))) then
-				player:GossipMenuAddItem(0, '  æ Arma de asta 400 æ  ', 1, 17)
-			end
-			if ((player:HasSpell(BALLESTAS_SPELL) and (player:GetSkillValue(BALLESTAS_SKILL) ~= 400))) then
-				player:GossipMenuAddItem(0, '  æ Ballestas 400 æ  ', 1, 18)
-			end
-			if ((player:HasSpell(BASTONES_SPELL) and (player:GetSkillValue(BASTONES_SKILL) ~= 400))) then
-				player:GossipMenuAddItem(0, '  æ Bastones 400 æ  ', 1, 19)
-			end
-			if ((player:HasSpell(ARMAS_DE_FUEGO_SPELL) and (player:GetSkillValue(ARMAS_DE_FUEGO_SKILL) ~= 400))) then
-				player:GossipMenuAddItem(0, '  æ Armas de fuego 400 æ  ', 1, 20)
-			end
-			if ((player:HasSpell(ARMAS_ARROJADIZAS_SPELL) and (player:GetSkillValue(ARMAS_ARROJADIZAS_SKILL) ~= 400))) then
-				player:GossipMenuAddItem(0, '  æ Armas arrojadizas 400 æ  ', 1, 21)
-			end
-			if ((player:HasSpell(MAZAS_SPELL) and (player:GetSkillValue(MAZAS_SKILL) ~= 400))) then
-				player:GossipMenuAddItem(0, '  æ Mazas de 1 mano 400 æ  ', 1, 22)
-			end
-			if ((player:HasSpell(MAZAS_DE_DOS_MANOS_SPELL) and (player:GetSkillValue(MAZAS_DE_DOS_MANOS_SKILL) ~= 400))) then
-				player:GossipMenuAddItem(0, '  æ Mazas de 2 manos 400 æ  ', 1, 23)
-			end
-			if ((player:HasSpell(VARITAS_SPELL) and (player:GetSkillValue(VARITAS_SKILL) ~= 400))) then
-				player:GossipMenuAddItem(0, '  æ Varitas manos 400 æ  ', 1, 25)
-			end
-			if ((player:HasSpell(SIN_ARMAS_SPELL) and (player:GetSkillValue(SIN_ARMAS_SKILL) ~= 400))) then
-				player:GossipMenuAddItem(0, '  æ Sin armas 400 æ  ', 1, 26)
-			end
-			player:GossipSendMenu(0x7FFFFFFF, object)
-		end
-	end
+    if (player:IsInCombat() == false) then
+        player:GossipClearMenu()
+        player:GossipSetText("Saludos |c990B0Bee$n|h|r. Bienvenido al Servidor. Mi objetivo es subirte la habilidad de armas hasta el 400. Pero primero necesitas visitar el instructor para aprender dicha habilidad si no la conoces.")
+        if (player:GetLevel() == 80) then
+            if ((player:HasSpell(ARCOS_SPELL) and (player:GetSkillValue(ARCOS_SKILL) ~= 400))) then
+                player:GossipMenuAddItem(0, '  æ Arcos 400 æ  ', 1, 10)
+            end
+            if ((player:HasSpell(DAGAS_SPELL) and (player:GetSkillValue(DAGAS_SKILL) ~= 400))) then
+                player:GossipMenuAddItem(0, '  æ Dagas 400 æ  ', 1, 11)
+            end
+            if ((player:HasSpell(ESPADAS_SPELL) and (player:GetSkillValue(ESPADAS_SKILL) ~= 400))) then
+                player:GossipMenuAddItem(0, '  æ Espadas de 1 mano 400 æ  ', 1, 12)
+            end
+            if ((player:HasSpell(ESPADAS_DE_DOS_MANOS_SPELL) and (player:GetSkillValue(ESPADAS_DE_DOS_MANOS_SKILL) ~= 400))) then
+                player:GossipMenuAddItem(0, '  æ Espadas de 2 manos 400 æ  ', 1, 13)
+            end
+            if ((player:HasSpell(HACHAS_SPELL) and (player:GetSkillValue(HACHAS_SKILL) ~= 400))) then
+                player:GossipMenuAddItem(0, '  æ Hachas de 1 mano 400 æ  ', 1, 14)
+            end
+            if ((player:HasSpell(HACHAS_DE_DOS_MANOS_SPELL) and (player:GetSkillValue(HACHAS_DE_DOS_MANOS_SKILL) ~= 400))) then
+                player:GossipMenuAddItem(0, '  æ Hachas de 2 manos 400 æ  ', 1, 15)
+            end
+            if ((player:HasSpell(DEFENSA_SPELL) and (player:GetSkillValue(DEFENSA_SKILL) ~= 400))) then
+                player:GossipMenuAddItem(0, '  æ Defensa 400 æ  ', 1, 16)
+            end
+            if ((player:HasSpell(ARMA_DE_ASTA_SPELL) and (player:GetSkillValue(ARMA_DE_ASTA_SKILL) ~= 400))) then
+                player:GossipMenuAddItem(0, '  æ Arma de asta 400 æ  ', 1, 17)
+            end
+            if ((player:HasSpell(BALLESTAS_SPELL) and (player:GetSkillValue(BALLESTAS_SKILL) ~= 400))) then
+                player:GossipMenuAddItem(0, '  æ Ballestas 400 æ  ', 1, 18)
+            end
+            if ((player:HasSpell(BASTONES_SPELL) and (player:GetSkillValue(BASTONES_SKILL) ~= 400))) then
+                player:GossipMenuAddItem(0, '  æ Bastones 400 æ  ', 1, 19)
+            end
+            if ((player:HasSpell(ARMAS_DE_FUEGO_SPELL) and (player:GetSkillValue(ARMAS_DE_FUEGO_SKILL) ~= 400))) then
+                player:GossipMenuAddItem(0, '  æ Armas de fuego 400 æ  ', 1, 20)
+            end
+            if ((player:HasSpell(ARMAS_ARROJADIZAS_SPELL) and (player:GetSkillValue(ARMAS_ARROJADIZAS_SKILL) ~= 400))) then
+                player:GossipMenuAddItem(0, '  æ Armas arrojadizas 400 æ  ', 1, 21)
+            end
+            if ((player:HasSpell(MAZAS_SPELL) and (player:GetSkillValue(MAZAS_SKILL) ~= 400))) then
+                player:GossipMenuAddItem(0, '  æ Mazas de 1 mano 400 æ  ', 1, 22)
+            end
+            if ((player:HasSpell(MAZAS_DE_DOS_MANOS_SPELL) and (player:GetSkillValue(MAZAS_DE_DOS_MANOS_SKILL) ~= 400))) then
+                player:GossipMenuAddItem(0, '  æ Mazas de 2 manos 400 æ  ', 1, 23)
+            end
+            if ((player:HasSpell(VARITAS_SPELL) and (player:GetSkillValue(VARITAS_SKILL) ~= 400))) then
+                player:GossipMenuAddItem(0, '  æ Varitas manos 400 æ  ', 1, 25)
+            end
+            if ((player:HasSpell(SIN_ARMAS_SPELL) and (player:GetSkillValue(SIN_ARMAS_SKILL) ~= 400))) then
+                player:GossipMenuAddItem(0, '  æ Sin armas 400 æ  ', 1, 26)
+            end
+            player:GossipSendMenu(0x7FFFFFFF, object)
+        end
+    end
 end
 
 function OnGossipSelect(event, player, object, sender, intid, code, menuid)
-	if (intid == 10) then
-		maxSkill = player:GetMaxSkillValue(ARCOS_SKILL)
-		player:AdvanceSkill(ARCOS_SKILL, maxSkill)
-	end
-	if (intid == 11) then
-		maxSkill = player:GetMaxSkillValue(DAGAS_SKILL)
-		player:AdvanceSkill(DAGAS_SKILL, maxSkill)
-	end
-	if (intid == 12) then
-		maxSkill = player:GetMaxSkillValue(ESPADAS_SKILL)
-		player:AdvanceSkill(ESPADAS_SKILL, maxSkill)
-	end
-	if (intid == 13) then
-		maxSkill = player:GetMaxSkillValue(ESPADAS_DE_DOS_MANOS_SKILL)
-		player:AdvanceSkill(ESPADAS_DE_DOS_MANOS_SKILL, maxSkill)
-	end
-	if (intid == 14) then
-		maxSkill = player:GetMaxSkillValue(HACHAS_SKILL)
-		player:AdvanceSkill(HACHAS_SKILL, maxSkill)
-	end
-	if (intid == 15) then
-		maxSkill = player:GetMaxSkillValue(HACHAS_DE_DOS_MANOS_SKILL)
-		player:AdvanceSkill(HACHAS_DE_DOS_MANOS_SKILL, maxSkill)
-	end
-	if (intid == 16) then
-		maxSkill = player:GetMaxSkillValue(DEFENSA_SKILL)
-		player:AdvanceSkill(DEFENSA_SKILL, maxSkill)
-	end
-	if (intid == 17) then
-		maxSkill = player:GetMaxSkillValue(ARMA_DE_ASTA_SKILL)
-		player:AdvanceSkill(ARMA_DE_ASTA_SKILL, maxSkill)
-	end
-	if (intid == 18) then
-		maxSkill = player:GetMaxSkillValue(BALLESTAS_SKILL)
-		player:AdvanceSkill(BALLESTAS_SKILL, maxSkill)
-	end
-	if (intid == 19) then
-		maxSkill = player:GetMaxSkillValue(BASTONES_SKILL)
-		player:AdvanceSkill(BASTONES_SKILL, maxSkill)
-	end
-	if (intid == 20) then
-		maxSkill = player:GetMaxSkillValue(ARMAS_DE_FUEGO_SKILL)
-		player:AdvanceSkill(ARMAS_DE_FUEGO_SKILL, maxSkill)
-	end
-	if (intid == 21) then
-		maxSkill = player:GetMaxSkillValue(ARMAS_ARROJADIZAS_SKILL)
-		player:AdvanceSkill(ARMAS_ARROJADIZAS_SKILL, maxSkill)
-	end
-	if (intid == 22) then
-		maxSkill = player:GetMaxSkillValue(MAZAS_SKILL)
-		player:AdvanceSkill(MAZAS_SKILL, maxSkill)
-	end
-	if (intid == 23) then
-		maxSkill = player:GetMaxSkillValue(MAZAS_DE_DOS_MANOS_SKILL)
-		player:AdvanceSkill(MAZAS_DE_DOS_MANOS_SKILL, maxSkill)
-	end
-	if (intid == 25) then
-		maxSkill = player:GetMaxSkillValue(VARITAS_SKILL)
-		player:AdvanceSkill(VARITAS_SKILL, maxSkill)
-	end
-	if (intid == 26) then
-		maxSkill = player:GetMaxSkillValue(SIN_ARMAS_SKILL)
-		player:AdvanceSkill(SIN_ARMAS_SKILL, maxSkill)
-	end
-	player:GossipComplete()
+    if (intid == 10) then
+        maxSkill = player:GetMaxSkillValue(ARCOS_SKILL)
+        player:AdvanceSkill(ARCOS_SKILL, maxSkill)
+    end
+    if (intid == 11) then
+        maxSkill = player:GetMaxSkillValue(DAGAS_SKILL)
+        player:AdvanceSkill(DAGAS_SKILL, maxSkill)
+    end
+    if (intid == 12) then
+        maxSkill = player:GetMaxSkillValue(ESPADAS_SKILL)
+        player:AdvanceSkill(ESPADAS_SKILL, maxSkill)
+    end
+    if (intid == 13) then
+        maxSkill = player:GetMaxSkillValue(ESPADAS_DE_DOS_MANOS_SKILL)
+        player:AdvanceSkill(ESPADAS_DE_DOS_MANOS_SKILL, maxSkill)
+    end
+    if (intid == 14) then
+        maxSkill = player:GetMaxSkillValue(HACHAS_SKILL)
+        player:AdvanceSkill(HACHAS_SKILL, maxSkill)
+    end
+    if (intid == 15) then
+        maxSkill = player:GetMaxSkillValue(HACHAS_DE_DOS_MANOS_SKILL)
+        player:AdvanceSkill(HACHAS_DE_DOS_MANOS_SKILL, maxSkill)
+    end
+    if (intid == 16) then
+        maxSkill = player:GetMaxSkillValue(DEFENSA_SKILL)
+        player:AdvanceSkill(DEFENSA_SKILL, maxSkill)
+    end
+    if (intid == 17) then
+        maxSkill = player:GetMaxSkillValue(ARMA_DE_ASTA_SKILL)
+        player:AdvanceSkill(ARMA_DE_ASTA_SKILL, maxSkill)
+    end
+    if (intid == 18) then
+        maxSkill = player:GetMaxSkillValue(BALLESTAS_SKILL)
+        player:AdvanceSkill(BALLESTAS_SKILL, maxSkill)
+    end
+    if (intid == 19) then
+        maxSkill = player:GetMaxSkillValue(BASTONES_SKILL)
+        player:AdvanceSkill(BASTONES_SKILL, maxSkill)
+    end
+    if (intid == 20) then
+        maxSkill = player:GetMaxSkillValue(ARMAS_DE_FUEGO_SKILL)
+        player:AdvanceSkill(ARMAS_DE_FUEGO_SKILL, maxSkill)
+    end
+    if (intid == 21) then
+        maxSkill = player:GetMaxSkillValue(ARMAS_ARROJADIZAS_SKILL)
+        player:AdvanceSkill(ARMAS_ARROJADIZAS_SKILL, maxSkill)
+    end
+    if (intid == 22) then
+        maxSkill = player:GetMaxSkillValue(MAZAS_SKILL)
+        player:AdvanceSkill(MAZAS_SKILL, maxSkill)
+    end
+    if (intid == 23) then
+        maxSkill = player:GetMaxSkillValue(MAZAS_DE_DOS_MANOS_SKILL)
+        player:AdvanceSkill(MAZAS_DE_DOS_MANOS_SKILL, maxSkill)
+    end
+    if (intid == 25) then
+        maxSkill = player:GetMaxSkillValue(VARITAS_SKILL)
+        player:AdvanceSkill(VARITAS_SKILL, maxSkill)
+    end
+    if (intid == 26) then
+        maxSkill = player:GetMaxSkillValue(SIN_ARMAS_SKILL)
+        player:AdvanceSkill(SIN_ARMAS_SKILL, maxSkill)
+    end
+    player:GossipComplete()
 end
 
 RegisterCreatureGossipEvent(NPC_ENTRY, 1, OnGossipHello)
