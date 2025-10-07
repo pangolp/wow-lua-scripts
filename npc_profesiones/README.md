def6ea4f5f8fb3f6ce79c7b6f5960808a9667105
Walter Pagani <paganiwalter@gmail.com>
2025-10-07 02:19:13 -0300

    Chore (Lua/SQL) update npc_profession (#2)
    * Rename folder to npc_profesiones
    
    * Delete unnecessary folders
    
    * tabs for spaces
    
    * update sql
    
    * upgrade lua
    
    * rename and update code
    
    * update lua

---
+++ npc_profesiones/README.md
@@ -0,0 +1,5 @@
+# ![logo](https://raw.githubusercontent.com/azerothcore/azerothcore.github.io/master/images/logo-github.png) AzerothCore
+
+# NPC Professions
+
+The NPC's job is basically to check that the player is level 80, and then raise their profession skills up to 450. What the player must do is go to the profession instructor (to prevent a player from having more than 2 professions, because if not, we should control how many professions they have), and once the profession level is less than 450, the NPC is in charge of raising it to 450, but without teaching them recipes.
---
+++ npc_profesiones/npc_professions.sql
@@ -0,0 +1,11 @@
+DELETE FROM `creature_template` WHERE `entry`=200004;
+INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
+(200004, 0, 0, 0, 0, 0, 'Oscar Isidro Parrilli', 'Profesiones 450', NULL, 0, 83, 83, 2, 35, 1, 1, 1, 1, 1, 20, 1, 3, 0, 35, 2000, 2000, 1, 1, 1, 768, 2048, 0, 0, 0, 0, 0, 0, 6, 36, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 2000, 1, 1, 1, 0, 0, 1, 0, 0, 2, '', 12340);
+
+DELETE FROM `creature_template_model` WHERE `CreatureID`=200004;
+INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
+(200004, 0, 18718, 1, 1, 0);
+
+DELETE FROM `creature` WHERE `id1`=200004;
+INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`, `CreateObject`, `Comment`) VALUES
+(4000004, 200004, 0, 0, 571, 0, 0, 1, 1, 0, 5787.18, 450.444, 658.782, 6.21808, 300, 0, 0, 27890000, 0, 0, 0, 0, 0, '', 0, 0, '');
